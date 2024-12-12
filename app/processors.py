ee81d0c020c2d1f51864fcdf03f450bb45927a28
Vasilis Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2024-12-12 02:23:28 +0100

    replace speech_recognition, add ig support - probably unfinished
    - replace speech_recognition with whisper python library
    - add instagram support

--- app/processors.py
+++ app/processors.py
@@ -1,9 +1,10 @@
 import json
 import os
 import re
-from typing import Any
-import speech_recognition as sr
+import whisper
 import subprocess
+from typing import Any
+from urllib.parse import urlparse
 
 def check_status(channel, id):
     folderPath = f"./{channel}/{id}/"
@@ -24,17 +25,42 @@ def get_audio(folderPath, youtubeUrl):
     subprocess.run(f"{cmd}", shell=True)
     return audioFile
 
+def get_channel(url):
+    if (urlparse(url).netloc.endswith('instagram.com')):
+        return "ig"
+    if (get_youtube_id(url) != ""):
+        return "yt"
+    return ""
+
 def get_text(audioFile, language : Any | None = None):
-    r = sr.Recognizer()
-    with sr.AudioFile(audioFile) as source:
-        data = r.record(source)
-        text = r.recognize_whisper(audio_data=data, language=language)
-        return text
+    model = whisper.load_model("turbo")
+
+    # load audio and pad/trim it to fit 30 seconds
+    audio = whisper.load_audio(audioFile)
+    audio = whisper.pad_or_trim(audio)
+
+    # make log-Mel spectrogram and move to the same device as the model
+    mel = whisper.log_mel_spectrogram(audio).to(model.device)
+
+    # detect the spoken language
+    _, probs = model.detect_language(mel)
+    print(f"Detected language: {max(probs, key=probs.get)}")
+
+    # decode the audio
+    options = whisper.DecodingOptions()
+    result = whisper.decode(model, mel, options)
+
+    # print the recognized text
+    print(result.text)
+    return result.text
 
 def get_video_lang(video_lang):
     if (video_lang == 'None'): return None
     if (video_lang == ''): return None
     return video_lang
+
+def get_instagram_id(url):
+    return urlparse(url).path
 
 def get_youtube_id(url):
     try:
@@ -43,15 +69,28 @@ def get_youtube_id(url):
     except:
         return ""
 
+def channel_supported(channel):
+    if (channel == "yt"):
+        return True
+    if (channel == "ig"):
+        return True
+    return False
+
 def run_process_in_background(channel, id, video_lang : Any | None = None):
     folderPath = f"./{channel}/{id}/"
     fileName = "index.json"
-    if (channel != "yt"):
+    if (channel_supported(channel) == False):
         save_file(folderPath, fileName, json.dumps({ "id": id, "status": f'{channel} is not supported' }))
     
     save_file(folderPath, fileName, json.dumps({ "channel": channel, "id": id, "status": "generating audio file" }))
-    youtubeUrl = f'https://youtu.be/{id}'
-    audioFile = get_audio(folderPath, youtubeUrl)
+
+    source_url = ""
+    if (channel == "yt"):
+        source_url = f'https://youtu.be/{id}'
+    if (channel == "ig"):
+        source_url = f'https://instagram.com/{id}'
+    
+    audioFile = get_audio(folderPath, source_url)
     save_file(folderPath, fileName, json.dumps({ "channel": channel, "id": id, "status": "generating text" }))
     text = get_text(audioFile, video_lang)
     save_file(folderPath, fileName, json.dumps({ "channel": channel, "id": id, "status": "done", "text": text }))
